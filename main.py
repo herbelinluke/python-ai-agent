f683bb17d6a476bb8212ec29446919298ab8861d
Luke Herbelin <herbelinluke@gmail.com>
2025-06-24 22:06:51 -0700

    accepts command line arguments

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
+    if len(sys.argv) <= 1:
+        print("you need to add a prompt")
+        sys.exit(1)
+    user_prompt = sys.argv[1]
 
-response = client.models.generate_content(model="gemini-2.0-flash-001",
-                                          contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    messages = [
+            types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    
+    response = client.models.generate_content(
+                model="gemini-2.0-flash-001",
+                contents=messages,
+    )
+    
+    print(response.text)
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count} \nResponse tokens: {response.usage_metadata.candidates_token_count}")
 
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count} \nResponse tokens: {response.usage_metadata.candidates_token_count}")
+if __name__ == "__main__":
+    main()
